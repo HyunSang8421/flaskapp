08babca1c31d062f0003925f25d8715cc3f3fbb6
HyungSang8421 <128012383+HyungSang8421@users.noreply.github.com>
2023-07-25 10:49:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-# from flask import Flask
-# app = Flask(__name__)
-
- 
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, Flask'
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-   return 'This page is an Index page.'
+ 
 
-if __name__ == '__main__':  
-   app.run('0.0.0.0', port=5000, debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask'
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#    return 'This page is an Index page.'
+
+# if __name__ == '__main__':  
+#    app.run('0.0.0.0', port=5000, debug=True)
